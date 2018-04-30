e51d12fb08ebbbc8da12b7a9f21734dcbcd4c64a
dominico966 <dominico966@gmail.com>
2018-04-30 16:15:31 +0900

    2차 병합 완료

--- app/src/main/java/com/ljy/musicplayer/biomusicplayer/ListViewItemSuggest.java
+++ app/src/main/java/com/ljy/musicplayer/biomusicplayer/ListViewItemSuggest.java
@@ -1,6 +1,8 @@
 package com.ljy.musicplayer.biomusicplayer;
 
-
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class ListViewItemSuggest extends ListViewItem {
 
@@ -40,6 +42,8 @@ public class ListViewItemSuggest extends ListViewItem {
     }
 
 
-
-
+    @Override
+    public View getView(LayoutInflater inflater, ViewGroup parent) {
+        return null;
+    }
 }
