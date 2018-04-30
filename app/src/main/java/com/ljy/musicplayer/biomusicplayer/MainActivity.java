ac868ea513ad860eca9bc01af489d8bcba513271
khw <shortleader@naver.com>
2018-04-30 15:04:31 +0900

    그래프에 더미 데이터 추가 / 구현 감정별 Top3 더미 추가 UI 색깔 변경

--- app/src/main/java/com/ljy/musicplayer/biomusicplayer/MainActivity.java
+++ app/src/main/java/com/ljy/musicplayer/biomusicplayer/MainActivity.java
@@ -1,9 +1,9 @@
 package com.ljy.musicplayer.biomusicplayer;
 
-import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -17,31 +17,34 @@ public class MainActivity extends AppCompatActivity {
 
         //사용자이름
         setTitle("홍길동");
+        getSupportActionBar().setBackgroundDrawable(getDrawable(R.drawable.bg_gradient));
+
 
         ViewPager viewPager = findViewById(R.id.viewPager);
-        TabLayout tabLayout = findViewById(R.id.tabs);
+        TabLayout tabLayout =findViewById(R.id.tabs);
+
 
         TabAdapter adapter = new TabAdapter(getSupportFragmentManager());
 
+
         //탭 추가
-        adapter.addFragment(new Tab1Fragment(), "Tab1");
-        adapter.addFragment(new Tab2Fragment(), "Tab2");
+        adapter.addFragment(new Tab1Fragment(),"Tab1");
+        adapter.addFragment(new Tab2Fragment(),"Tab2");
         viewPager.setAdapter(adapter);
 
         tabLayout.setupWithViewPager(viewPager);
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.menu, menu);
+        getMenuInflater().inflate(R.menu.menu,menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.menu_face_scan) {
+        if (item.getItemId()==R.id.menu_face_scan) {
             Toast.makeText(this, "얼굴인식", Toast.LENGTH_SHORT).show();
 
         }
